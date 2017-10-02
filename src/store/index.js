635237e1258af1875851faea49e13de20e749b78
Vladislav <Vladislav.Yurgel@r-stylelab.com>
2017-10-02 18:14:34 +0300

    Added saving state after updated web page

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import * as Cookies from 'js-cookie'
 
 import drawer from './modules/drawer'
 import user from './modules/user'
@@ -7,8 +9,11 @@ import user from './modules/user'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+    plugins: [
+        createPersistedState()
+    ],
     modules: {
         drawer: drawer,
         user: user
-    }
+    },
 });
